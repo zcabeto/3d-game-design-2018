5e9f2850504335a423de17c941882e1a4f1ed386
zcabeto <zcabeto@ucl.ac.uk>
2022-10-18 23:34:58 +0100

    added files and assets

---
+++ gameParts/Assets/Scripts/CamMove.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour {
+
+    public GameObject cam;
+    public GameObject D;
+    private Vector3 pos;
+    private Vector3 Dpos;
+    private bool hit = false;
+    private Vector3 angle;
+    private float difference;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Dpos = D.gameObject.transform.position; // Damian's place
+        pos = gameObject.transform.position; // Pointer's place
+
+        difference = Dpos.x - pos.x;
+
+        if (hit == false)
+        {
+            pos.x = Dpos.x - 6; //setting the pointer at the right distance
+        }
+        else
+        {
+            if ((difference) >= 6)
+            {
+                hit = false; //when the distance is back to normal
+            }
+        }
+
+        pos.z = Dpos.z; //always the same y and z
+        pos.y = Dpos.y + 3;
+
+        gameObject.transform.position = pos; //move pointer
+        cam.gameObject.transform.position = pos;  //move camera w/it
+
+        if ((difference) <= 3.5) //angle changing
+        {
+            angle = cam.gameObject.transform.eulerAngles;
+            angle.x = (((3.5f - difference) * 10) + 15);
+            cam.gameObject.transform.eulerAngles = angle;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            hit = true;
+        }
+    }
+}
---
+++ gameParts/Assets/Scripts/move.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    public GameObject Camera;
+    private Vector3 floorPos;
+    private Vector3 pos;
+    private Vector3 floorScale;
+    private Vector3 scale;
+	// Update is called once per frame
+	void Update ()
+    {
+        gameObject.transform.eulerAngles = new Vector3 (0f,0f,0f);
+        Vector3 camPos = Camera.gameObject.transform.position;
+        Vector3 pos = gameObject.transform.position;
+		if (Input.GetKey(KeyCode.W) == true)
+        {
+            if (pos.x <= 149)
+            {
+                pos.x += 0.1f;
+            }
+        }
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+            if (pos.x >= -149)
+            {
+                pos.x -= 0.1f;
+            }
+        }
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            if (pos.z < 99)
+            {
+                pos.z += 0.1f;
+            }
+        }
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            if (pos.z > -99)
+            {
+                pos.z -= 0.1f;
+            }
+        }
+        gameObject.transform.position = pos;
+        Camera.gameObject.transform.position = camPos;
+	}
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "floor")
+        {
+            floorPos = collision.gameObject.transform.position;
+            pos = gameObject.transform.position;
+            floorScale = collision.gameObject.transform.localScale;
+            scale = gameObject.transform.localScale;
+            
+            if ((floorPos.y + (floorScale.y / 2))-(pos.y - (scale.y / 2)) < 2)
+            {
+                pos.y = (floorPos.y + (floorScale.y / 2)) + (scale.y);
+            }
+            gameObject.transform.position = pos;
+        } 
+    }
+}
